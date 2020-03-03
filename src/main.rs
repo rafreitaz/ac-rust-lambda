eeb2ead27613f68e7737cb7eafa674eea957b05c
rfreitas <rfreitas@squaretrade.com>
2020-03-03 20:40:34 -0300

    Configure Lambda + Api Gateway basic function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    lambda!(api_gateway_handler);
+
+    Ok(())
+}
+
+pub fn api_gateway_handler(_request: String, _c: Context) -> Result<String, HandlerError> {
+    let response = "{\"statusCode\": 200}".to_string();
+
+    Ok(response)
 }
